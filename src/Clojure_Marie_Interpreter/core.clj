e052c98b2f25dec785fd296f6d2f21f05b10ffa7
Heiðar Þórðarson <heidarlitli@gmail.com>
2012-01-08 02:24:16 +0000

    Lein project

---
+++ src/Clojure_Marie_Interpreter/core.clj
@@ -0,0 +1,1 @@
+(ns Clojure-Marie-Interpreter.core)
---
+++ test/Clojure_Marie_Interpreter/test/core.clj
@@ -0,0 +1,6 @@
+(ns Clojure-Marie-Interpreter.test.core
+  (:use [Clojure-Marie-Interpreter.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
